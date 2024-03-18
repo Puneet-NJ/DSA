f39780f64cedc9ac584cabe80c80b711922e6cbd
Puneet NJ <lpuneetnj@gmail.com>
2024-03-18 15:25:54 +0530

    Added 2 questiona on HashMap i) Top K Frequent Elements ii) Product Of Array Except Self

--- HashMaps/src/Practice.java
+++ HashMaps/src/Practice.java
@@ -2,16 +2,22 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-//        Map<String, Integer> map = new HashMap<>();
+////        Map<String, Integer> map = new HashMap<>();
+////
+////        map.put("a", 10);
+////        map.put("b", 20);
+////
+////        System.out.println(map.remove("a"));
+////
+////        System.out.println(map);
 //
-//        map.put("a", 10);
-//        map.put("b", 20);
+////        char ch = 'a';
+////        System.out.println(ch - 96);
 //
-//        System.out.println(map.remove("a"));
+//        int[][] a = new int[3][2];
 //
-//        System.out.println(map);
-
-        char ch = 'a';
-        System.out.println(ch - 96);
+//        for (int i = 0; i < a.length; i++) {
+//            System.out.println(Arrays.toString(a[i]));
+//        }
     }
 }
